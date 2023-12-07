4fe87dbc7d9d4b4c50261c6f22a3e237eb14326e
Christian Carlson <christiancarlson648@gmail.com>
2023-12-07 14:03:53 -0600

    Did things

--- src/components/FavoritesView/FavoritesView.jsx
+++ src/components/FavoritesView/FavoritesView.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./FavoritesView.css";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -5,7 +6,19 @@ export default function FavoritesView() {
   const getImages = () => {
     const dispatch = useDispatch();
 
-    const imgReducer = useSelector((store) => store);
+    const imgReducer = useSelector((store) => store.getFavorites);
+
+    useEffect =
+      (() => {
+        getFavPictures();
+      },
+      []);
+
+    const getFavPictures = () => {
+      dispatch({
+        type: "GET_FAVORITES",
+      });
+    };
   };
   return (
     <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,17 @@ import {
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
+import { takeLatest, put } from "redux-saga/effects";
 
 const searchResults = (state = [], action) => {
   if (action.type === "SET_SEARCH_RESULTS") {
+    return action.payload;
+  }
+  return state;
+};
+
+const getFavorites = (state = [], action) => {
+  if (action.type === "GET_FAVORITES") {
     return action.payload;
   }
   return state;
@@ -19,6 +27,7 @@ const saga = createSagaMiddleware();
 const store = createStore(
   combineReducers({
     searchResults,
+    getFavorites,
   }),
   applyMiddleware(logger, saga),
 );
