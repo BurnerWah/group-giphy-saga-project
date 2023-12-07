c3f8600bf9276848e58d350483387878c218cb37
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-07 13:51:56 -0600

    Added Get Categories saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -12,6 +12,16 @@ function* search(action) {
   }
 }
 
+function* getCategories() {
+  try {
+    const response = yield axios.get("/api/categories");
+    yield put({ type: "SET_CATEGORIES", payload: response.data });
+  } catch (error) {
+    console.log("Error with categories request:", error);
+  }
+}
+
 export default function* rootSaga() {
   yield takeLatest("SAGA/SEARCH", search);
+  yield takeLatest("SAGA/GET_CATEGORIES", getCategories);
 }
