a83925fd749c05bba2841eec304fbf565c45538e
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-07 13:50:56 -0600

    Added search saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,1 +1,17 @@
-export default function* rootSaga() {}
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
+
+function* search(action) {
+  try {
+    const response = yield axios.get("/api/search", {
+      params: { q: action.payload },
+    });
+    yield put({ type: "SET_SEARCH_RESULTS", payload: response.data });
+  } catch (error) {
+    console.log("Error with search request:", error);
+  }
+}
+
+export default function* rootSaga() {
+  yield takeLatest("SAGA/SEARCH", search);
+}
