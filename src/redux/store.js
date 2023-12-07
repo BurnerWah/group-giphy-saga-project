ec868635ce84c75253f1e270799ce3aa1b0c38d5
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-07 13:40:59 -0600

    Created redux store with empty sagas

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+
+const searchResults = (state = [], action) => {
+  if (action.type === "SET_SEARCH_RESULTS") {
+    return action.payload;
+  }
+  return state;
+};
+
+const saga = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({
+    searchResults,
+  }),
+  applyMiddleware(logger, saga),
+);
+
+saga.run(rootSaga);
+
+export default store;
