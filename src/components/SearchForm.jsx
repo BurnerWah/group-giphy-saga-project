eff0152c7c13bf2456b769b67574d2fb112684d0
Braden Soller <braden.soller18@gmail.com>
2023-12-07 15:46:25 -0600

    completed feature/frontend-search-redux

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SearchFrom() {
+  //input expects a string
   const [search, setSearch] = useState("");
 
-  console.log(search);
+  const dispatch = useDispatch();
 
+  //Gets reducer from the store
+  const searchResults = useSelector((store) => store.searchResults);
+
+  //setting the input value to whatever string we toyped in
   const handleSearch = (event) => {
     setSearch(event.target.value);
   };
-
+  //calling our get function to get the results we searched
   const newSearch = (event) => {
     event.preventDefault();
+    dispatch({ type: "SAGA/SEARCH", payload: search });
   };
+
+  // const setFavorite = () => {
+
+  // }
 
   return (
     <div>
@@ -23,7 +34,14 @@ export default function SearchFrom() {
           onChange={handleSearch}
         />
         <button>search</button>
+        {/* mapping through are search results we held in our reducer and rending the img */}
       </form>
+      {searchResults.map((gif) => (
+        <div>
+          <img src={gif.images.fixed_height.url}></img>
+          {/* <button onClick={setFavorite}>favorite</button> */}
+        </div>
+      ))}
     </div>
   );
 }
