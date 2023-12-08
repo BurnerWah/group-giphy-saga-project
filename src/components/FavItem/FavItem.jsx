7697bd3f9d3bcf435b3c1481259cc289298a0845
Christian Carlson <christiancarlson648@gmail.com>
2023-12-08 11:43:49 -0600

    Didn't actually do CSS stuff, despite the name, but actually commented

--- src/components/FavItem/FavItem.jsx
+++ src/components/FavItem/FavItem.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function FavItem({ favorite }) {
+  // 👇 useState is set to favorite.category_id, so that it appears on load
   const [category, setCategory] = useState(favorite.category_id);
   const dispatch = useDispatch(0);
   const categories = useSelector((store) => store.categories);
 
+  // Handles submit.
   const handleSubmit = (e) => {
     e.preventDefault();
+    // 👇 dispatches to the SAGA that holds the category data
     dispatch({
       type: "SAGA/SET_CATEGORY",
+      // 👇 sets the cateory for the specific item by using its ID
       payload: { id: favorite.id, category: category },
     });
   };
@@ -19,6 +23,7 @@ export default function FavItem({ favorite }) {
         src={`https://media1.giphy.com/media/${favorite.giphy_id}/200.gif`}
       ></img>
       <form onSubmit={handleSubmit}>
+        {/* drop-down menu */}
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value={0}>none</option>
           {categories.map((category) => {
